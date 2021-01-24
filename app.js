7636b04025de334afd1eaf5c44516e37453b05db
AbanobYoussef <abanobyoussef1996@gmail.com>
2021-01-24 02:29:36 +0200

    Saving Data

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const cafeList = document.querySelector('#cafe-list');
-
+const form = document.querySelector('#add-cafe-form');
 
 // create element & render cafe
-function renderCafe(doc) {
+function renderCafa(doc) {
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
@@ -17,8 +17,23 @@ function renderCafe(doc) {
     cafeList.appendChild(li);
 }
 
-db.collection('Cafa').get().then(snapshot => {
-    snapshot.docs.forEach(doc => {
-        renderCafe(doc);
+function Load() {
+    db.collection('Cafa').get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            renderCafa(doc);
+        });
     });
+}
+Load();
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('Cafa').add({
+        Name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+    cafeList.innerHTML = '';
+    Load();
 });
