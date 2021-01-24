30fa36e95fff73548d808cddd6b291454a2d0379
AbanobYoussef <abanobyoussef1996@gmail.com>
2021-01-24 03:36:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ function renderCafa(doc) { // as a for loop
 
     // deleting data
     cross.addEventListener('click', (e) => {
-        e.stopPropagation();
         let id = e.target.parentElement.getAttribute('data-id');
         db.collection('Cafa').doc(id).delete();
         cafeList.innerHTML = '';
